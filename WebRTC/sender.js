15a66fc7f0309252088eb2009ed162b34d8f8dcb
MantaeRae <jtlanagan@gmail.com>
2025-02-13 17:18:36 +0800

    moved creation of ice candidates

--- WebRTC/sender.js
+++ WebRTC/sender.js
@@ -50,82 +50,76 @@ socket.onmessage = async (event) => {
 
 // Capture video and create offer
 async function createPeerConnection() {
+  const pc = new RTCPeerConnection();
 
-    const pc = new RTCPeerConnection();
+  pc.onicecandidate = (event) => {
+    if (event.candidate) {
+      socket.send(JSON.stringify({
+        type: "candidate",
+        candidate: event.candidate,
+        target: receiverName
+      }));
+    }
+  };
 
-    // List devices and then filter videoinput ones
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    console.log("List of video devices:", videoDevices);
+  // List devices and then filter videoinput ones
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  console.log("List of video devices:", videoDevices);
 
 
-    // Loop through all found video inputs and send them over their own track
-    for (const [index, device] of videoDevices.entries()) {
+  // Loop through all found video inputs and send them over their own track
+  for (const [index, device] of videoDevices.entries()) {
 
-      const cameraConstraints = { video: { deviceId: device.deviceId,
-                                  width: { ideal: 640 }, 
-                                  height: { ideal: 480 }}, 
-                                  audio: false };
-      const stream = await navigator.mediaDevices.getUserMedia(cameraConstraints);
+    const cameraConstraints = { video: { deviceId: device.deviceId,
+                                width: { ideal: 640 }, 
+                                height: { ideal: 480 }}, 
+                                audio: false };
+    const stream = await navigator.mediaDevices.getUserMedia(cameraConstraints);
 
-      // Add each track to the peerConnection
-      for (const track of stream.getTracks()) {
-        const sender = pc.addTrack(track, stream);
+    // Add each track to the peerConnection
+    for (const track of stream.getTracks()) {
+      const sender = pc.addTrack(track, stream);
 
-        // Double checks that each track is a video track
-        if (track.kind === 'video') {
-          const parameters = sender.getParameters();
-          parameters.encodings[0].maxBitrate = 100000; // 0.1 Mbps
-          await sender.setParameters(parameters);
-        }
+      // Double checks that each track is a video track
+      if (track.kind === 'video') {
+        const parameters = sender.getParameters();
+        parameters.encodings[0].maxBitrate = 100000; // 0.1 Mbps
+        await sender.setParameters(parameters);
       }
-
-      // Add each track to the html stream
-      const videoElement = document.getElementById('camera${index + 1}Video');
-
-      // Optionally create a video element if it doesn't already exist
-      const newVideo = document.createElement('video');
-      newVideo.id = `camera${index + 1}Video`;
-      newVideo.autoplay = true;
-      newVideo.playsInline = true;
-      newVideo.srcObject = stream;
-      document.body.appendChild(newVideo);
     }
 
+    // Add each track to the html stream
+    const videoElement = document.getElementById('camera${index + 1}Video');
 
+    // Optionally create a video element if it doesn't already exist
+    const newVideo = document.createElement('video');
+    newVideo.id = `camera${index + 1}Video`;
+    newVideo.autoplay = true;
+    newVideo.playsInline = true;
+    newVideo.srcObject = stream;
+    document.body.appendChild(newVideo);
+  }
+  console.log("creating offer...");
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  
+  socket.send(
+    JSON.stringify({
+      type: "offer",
+      offer: pc.localDescription,
+      target: receiverName,
+    })
+  );
+  console.log("offer sent success");
 
-    console.log("creating offer...");
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
-    
-    socket.send(
-      JSON.stringify({
-        type: "offer",
-        offer: pc.localDescription,
-        target: receiverName,
-      })
-    );
-    console.log("offer sent success");
-
-    return pc;
+  return pc;
 }
 
 createPeerConnection().then((pc) => {
   peerConnection = pc;
   console.log("New rtc session created");
 });
-
-
-// Handle ICE Candidates: ICE candadites checks the best connection between the two peers. It fires continuously throughout the connection, checking and changing the connection.
-peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-        socket.send(JSON.stringify({
-            type: "candidate",
-            candidate: event.candidate,
-            target: receiverName
-        }));
-    }
-};
 
 
 
