7a097ed682dd05a6ba92a778940f734786f63f2d
MantaeRae <jtlanagan@gmail.com>
2025-02-19 17:04:09 +0800

    added an autoplay to the video element

--- WebRTC/receiver.js
+++ WebRTC/receiver.js
@@ -96,6 +96,7 @@ async function acceptPeerConnection() {
       if (nextCamera === "left") {
         const newVideo = document.getElementById('video-left');
         newVideo.srcObject = new MediaStream([event.track]);
+        videoElement.play();
         console.log("Left video stream has started");
       }
       else {
