fb04d76f491b43930b6d7f4b43bfb1211e12a600
MantaeRae <jtlanagan@gmail.com>
2025-02-21 12:42:59 +0800

    updated bash file

--- WebRTC/execute.sh
+++ WebRTC/execute.sh
@@ -3,7 +3,7 @@
 
 # Replace with the correct paths to your HTML files
 echo "Starting sender scripts"
-node server.js &
+gnome-terminal -- bash -c "node server.js; exec bash" &
 firefox /home/uwa-rover/Documents/rover2025/communications-team/WebRTC/sender_main.html &
 firefox /home/uwa-rover/Documents/rover2025/communications-team/WebRTC/sender_periph.html &
 firefox /home/uwa-rover/Documents/rover2025/communications-team/WebRTC/receiver.html &
