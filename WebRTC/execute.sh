e6d39f2a61bdcc4c3dc11ecbcc527b4d63e2eee3
MantaeRae <jtlanagan@gmail.com>
2025-02-21 12:28:28 +0800

    created an execute file

---
+++ WebRTC/execute.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Opens two HTML files in Firefox
+
+# Replace with the correct paths to your HTML files
+echo "Starting sender scripts"
+firefox sender_main.html &
+firefox sender_periph.html &
