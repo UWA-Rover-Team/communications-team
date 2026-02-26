d68a5d6039057f7a5296d2e5d38bed385f480728
MantaeRae <jtlanagan@gmail.com>
2026-02-26 15:05:28 +0800

    Updated error checking for camera disconnect

--- basestationEnd/socketServer/src/server.ts
+++ basestationEnd/socketServer/src/server.ts
@@ -2,19 +2,42 @@ import WebSocket from 'ws';
 import { WebRTCMessage, clients } from './webRtcStreamObject';
 
 const wss = new WebSocket.Server({ host: "0.0.0.0", port: 8080 });
-const clientlist: { [key: string]: WebSocket } = {};
+const clientlist: { [key in clients]: WebSocket | null } = {
+    BASE_STATION: null,
+    ROVER: null,
+    SERVER: null
+};
+
 console.log("Server Started");
 
 wss.on("connection", (ws: WebSocket) => {
+    let registeredClientId: clients | null = null;
     ws.on("message", (message: WebSocket.Data) => {
         const data: WebRTCMessage = JSON.parse(message.toString());
 
         if (data.type === "REGISTER") {
             clientlist[data.client] = ws;
+            registeredClientId = data.client;
             console.log(`Client registered as ${data.client}`);
         }
         else if (clientlist[data.target]) {
             clientlist[data.target]?.send(message.toString());
+        }
+    });
+
+    ws.on('error', (err: NodeJS.ErrnoException) => {
+        if (err.code === 'ECONNRESET') {
+            console.log(`Client ${registeredClientId ?? 'unknown'} disconnected abruptly`);
+            if (registeredClientId) {
+                clientlist[registeredClientId] = null;
+            }
+
+            const registerMsg: WebRTCMessage = {
+                type: "ERROR",
+                client: "BASE_STATION",  
+                target: registeredClientId === "ROVER" ? "BASE_STATION" : "ROVER", // if rover, go base station, else rover
+                error: "DISCONNECT"
+            };
         }
     });
 });
--- basestationEnd/socketServer/src/webRtcStreamObject.ts
+++ basestationEnd/socketServer/src/webRtcStreamObject.ts
@@ -1,13 +1,24 @@
+
 export type clients = "BASE_STATION" | "ROVER" | "SERVER";
-export type cameras = "FRONT" | "BACK" | "LEFT" | "RIGHT" | "MANIP"
+
+export type clientError = "DISCONNECT"
+
+export enum cameras {
+    FRONT = 1,
+    BACK = 2,
+    LEFT = 3,
+    RIGHT = 4,
+    MANIP = 5
+}
 export type resolution = [width: number, height: number];
 
 export interface WebRTCMessage {
     client: clients;
-    type: "REGISTER" | "OFFER" | "ANSWER" | "ICE_CANDIDATE" | "CAMERA_REQUEST";
+    type: "REGISTER" | "OFFER" | "ANSWER" | "ICE_CANDIDATE" | "CAMERA_REQUEST" | "ERROR";
     target: clients;
     sdp?: string;
     candidate?: RTCIceCandidate;
     camera?: cameras;
     resolution?: resolution;
+    error?: clientError;
 }
